1cc08f211350047aac160c53ed64923492abaed3
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2023-07-04 20:03:26 -0300

    feat(rodape): estrutura do rodapé adicionada

--- frontmaster-site/src/app/shared/shared.module.ts
+++ frontmaster-site/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     MenuComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
